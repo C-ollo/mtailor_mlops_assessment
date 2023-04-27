172d80efc60a48d3b979dc41d65dd42c425445ac
C-ollo <103580135+C-ollo@users.noreply.github.com>
2023-04-27 14:36:16 +0300

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -3,8 +3,8 @@ import time
 import base64
 
 def test_server():
-    api_key = "8eb2c606-b6d1-4a61-aefe-d45b354e0ae4"
-    model_key = "15db684c-155a-4393-8fbc-e753e6348c7b"
+    api_key = ""
+    model_key = ""
     with open('n01440764_tench.jpeg', 'rb') as f:
         image_data = f.read()   
     image_data = base64.b64encode(image_data).decode('utf-8')
